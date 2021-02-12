6c3d2f8a36485bc0defd81812c57d69e2e7f8017
Eric <eric-muench@outlook.de>
2021-02-12 10:54:35 +0100

    further UI Improvements (changed search view)

--- app/src/main/java/de/ericmuench/appsfactorytesttask/ui/searchartist/SearchArtistFragment.kt
+++ app/src/main/java/de/ericmuench/appsfactorytesttask/ui/searchartist/SearchArtistFragment.kt
@@ -1,10 +1,9 @@
 package de.ericmuench.appsfactorytesttask.ui.searchartist
 
 import android.os.Bundle
+import android.view.*
+import android.widget.CheckBox
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
@@ -13,6 +12,7 @@ import de.ericmuench.appsfactorytesttask.R
 import de.ericmuench.appsfactorytesttask.databinding.FragmentSearchArtistBinding
 import de.ericmuench.appsfactorytesttask.ui.uicomponents.recyclerview.GenericSimpleItemAdapter
 import de.ericmuench.appsfactorytesttask.util.extensions.*
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass, which is responsible for the search of an artist.
@@ -21,6 +21,13 @@ class SearchArtistFragment : Fragment() {
 
     //fields
     private lateinit var viewBinding : FragmentSearchArtistBinding
+
+    //TODO: remove dummy data
+    val dummyData = List<String>(20){
+        "Item ${it+1}"
+    }.toMutableList()
+
+    private var adapter : GenericSimpleItemAdapter<String>? = null
 
     //lifecycle functions
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +38,7 @@ class SearchArtistFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View{
+        setHasOptionsMenu(true)
         viewBinding = FragmentSearchArtistBinding.inflate(inflater)
         return viewBinding.root
     }
@@ -39,64 +47,63 @@ class SearchArtistFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //setup layout
-        setupSearchComponents()
         setupRecyclerView()
     }
 
-    //help functions for Layout setup
-    /**
-     * This function does the UI-Setup of the SearchView and all its related components
-     */
-    private fun setupSearchComponents() = with(viewBinding){
-        searchviewSearchArtist.removeSearchPlate()
-        searchviewSearchArtist.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                hideKeyboard()
-                println("Submit Text: $query")
-                //TODO: add code for submitting the search text
-                return false
-            }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_artist_actionbar_menu,menu)
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                println("Text changed : $newText")
-                //TODO: Sync search Text with VM
-                return false
-            }
-        })
+        menu.findItem(R.id.search_artist_acbar_item_searchbar).actionView.castedAs<SearchView> {
+            it.removeSearchPlate()
+            it.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    hideKeyboard()
+                    println("Submit Text: $query")
+                    //TODO: add code for submitting the search text
+                    return false
+                }
 
-        imgbtnSearchArtist.setOnClickListener {
-            //TODO add code for submitting the search text
-            println("Submit Text: ${searchviewSearchArtist.query}")
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    println("Text changed : $newText")
+                    //TODO: Sync search Text with VM
+                    return false
+                }
+            })
+
         }
-
     }
 
-    private fun setupRecyclerView() = with(viewBinding.recyclerviewSearchArtist){
-        //TODO: remove dummy data
-        val dummyData = List<String>(20){
-            "Item ${it+1}"
+    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.acBarTest -> {
+                adapter?.addElement("new Data ${adapter?.itemCount ?: 0 + 1}")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }*/
 
+
+    //help functions for Layout setup
+    //TODO: Documentation
+    private fun setupRecyclerView() = with(viewBinding){
+        //setHasFixedSize(true)
         activity.notNull { act ->
-            layoutManager = if(act.runsInLandscape()) GridLayoutManager(act,2) else LinearLayoutManager(act)
-            adapter = GenericSimpleItemAdapter(act,dummyData).onApplyDataToViewHolder { holder, str ->
-                holder.txtText.text = str
-                holder.btnIcon.setIconResource(R.drawable.ic_album)
-                //holder.materialButtonTogleGroup.visibility = View.INVISIBLE
-                holder.materialButtonTogleGroup.addOnButtonCheckedListener { _, _, checked ->
-                    println("value of $str is now $checked")
-                }
-                holder.cardView.setOnClickListener {
-                    println("$str was clicked")
-                }
-            }
+            recyclerviewSearchArtist.layoutManager = if(act.runsInLandscape()) GridLayoutManager(act,2) else LinearLayoutManager(act)
+            adapter = GenericSimpleItemAdapter(act,dummyData)
+                .onApplyDataToViewHolder { holder, str, pos ->
+                    holder.txtText.text = str
+                    holder.checkBox.setButtonDrawable(R.drawable.item_stored_selector)
+                    holder.checkBox.setOnCheckedChangeListener { box, checked ->
+                        println("value of $str is now $checked")
+                    }
 
-            viewBinding.imgbtnSearchArtist.setOnClickListener {
-                //TODO remove this dummy code
-                adapter.castedAs<GenericSimpleItemAdapter<String>> { ada ->
-                    ada.addElements(List(5){"Another Item #${it + 1 }"})
+                    holder.cardView.setOnClickListener {
+                        println("$str was clicked")
+                    }
                 }
-            }
+
+            recyclerviewSearchArtist.adapter = adapter
         }
     }
 }
--- app/src/main/java/de/ericmuench/appsfactorytesttask/ui/uicomponents/recyclerview/GenericSimpleItemAdapter.kt
+++ app/src/main/java/de/ericmuench/appsfactorytesttask/ui/uicomponents/recyclerview/GenericSimpleItemAdapter.kt
@@ -4,14 +4,29 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.button.MaterialButtonToggleGroup
 import de.ericmuench.appsfactorytesttask.R
 
-//TODO: Documentation
+/**
+ * This typealias should facilitate the work with the functional type for the apply event.
+ * The type of this typealias should allow the access to the Adapter within the closure
+ * of onApplyDataToViewHolder of the class. Further to that, you can use the ViewHolder, the current
+ * element and the positions to apply your data to the basic UI-Components of the ViewHolder as
+ * you like.
+ */
+private typealias OnApplyEventListener<T>
+        = GenericSimpleItemAdapter<T>.(GenericSimpleItemAdapter.GenericSimpleItemViewHolder, T, Int) -> Unit
+
+/**
+ * This class defines a Recyclerview-Adapter for generic purposes to bind arbitrary elements to
+ * a simple Item. For this it uses an @see OnApplyEventListener<T> and Fluent-API-Syntax.
+ * This can be used to applying data to the ViewHolders UI-Components as you specify it in
+ * onApplyDataToViewHolder.
+ *
+ */
 class GenericSimpleItemAdapter<T>(
         private val context : Context,
         adapterData : Iterable<T>
@@ -24,7 +39,7 @@ class GenericSimpleItemAdapter<T>(
     /**
      * This field handles the bind operation for the ViewHolder and is called in @see onBindViewHolder.
      * */
-    private var onApplyDataToViewHolder : (GenericSimpleItemViewHolder,T) -> Unit = {_,_ -> }
+    private var onApplyDataToViewHolder : OnApplyEventListener<T> = {_,_,_ -> }
 
     //Functions for Adapter
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenericSimpleItemViewHolder {
@@ -34,7 +49,7 @@ class GenericSimpleItemAdapter<T>(
 
     override fun onBindViewHolder(holder: GenericSimpleItemViewHolder, position: Int) {
         val element = data[position]
-        onApplyDataToViewHolder.invoke(holder,element)
+        onApplyDataToViewHolder.invoke(this,holder,element,position)
     }
 
     override fun getItemCount(): Int = data.size
@@ -45,7 +60,7 @@ class GenericSimpleItemAdapter<T>(
      * @param applyFunc The value that should be contained in @see onApplyDataToViewHolder
      * @return this object to provide Fluent-Api-Syntax
      */
-    fun onApplyDataToViewHolder(applyFunc : (GenericSimpleItemViewHolder,T) -> Unit)
+    fun onApplyDataToViewHolder(applyFunc : OnApplyEventListener<T>)
         : GenericSimpleItemAdapter<T> {
         onApplyDataToViewHolder = applyFunc
         return this
@@ -81,7 +96,6 @@ class GenericSimpleItemAdapter<T>(
     class GenericSimpleItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val cardView: CardView = view.findViewById(R.id.card_generic_simple_item)
         val txtText: TextView = view.findViewById(R.id.txt_text_generic_simple_item)
-        val materialButtonTogleGroup: MaterialButtonToggleGroup = view.findViewById(R.id.mbtg_generic_simple_item)
-        val btnIcon: MaterialButton = view.findViewById(R.id.btn_icon_generic_simple_item)
+        val checkBox : CheckBox = view.findViewById(R.id.checkbox_generic_simple_item)
     }
 }
